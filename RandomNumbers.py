4d343d5ad35e4c52078fbaa80aa2f071b83140a9
itisjustmeeee <guzairovaolga21@gmail.com>
2025-04-10 14:34:40 +0300

    Update RandomNumbers.py
    something for dismath

--- RandomNumbers.py
+++ RandomNumbers.py
@@ -1,5 +1,12 @@
+'''
 from random import randint
 
 x = randint(1, 30)
 #change the range if needed
+print(x)
+'''
+'''
+x = (6*(1**2 + 2**2) + 1**2 + 2**2 + 1 + 2)
+'''
+x = (6*(1 + 4) + 1 + 4 + 1 + 2)
 print(x)
